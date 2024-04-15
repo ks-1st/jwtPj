a29771d4255d8c3638a5de1414d628f5976e608f
kso <klc4023@gmail.com>
2024-04-15 20:18:14 +0900

    Revert "reset"
    This reverts commit 9d0073788056d43e67a08123386edbd0aa461dc6.

---
+++ jwtdemo/src/main/java/com/example/jwtdemo/JwtdemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.jwtdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+
+@SpringBootApplication
+public class JwtdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtdemoApplication.class, args);
+	}
+
+}
+
+@RestController
+class Helloworld {
+        @GetMapping("/")
+        public String greet() {
+                return "Hello!";
+        }
+}
